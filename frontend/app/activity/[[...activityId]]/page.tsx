fdca531772d3ebc86dfe0540562b057d7930a4cc
Myles Spencer <mspencer.mes@gmail.com>
2024-05-05 09:48:51 -0700

    Basic activity sidebar

--- frontend/app/activity/[[...activityId]]/page.tsx
+++ frontend/app/activity/[[...activityId]]/page.tsx
@@ -4,9 +4,13 @@ import { useEffect, useState } from "react";
 import EventSummary, { Event } from '@/components/event-summary';
 import styles from './activity.module.scss';
 import { getAllActivity } from "../actions";
+import EventSidebar from "./event-sidebar";
+import { useRouter } from "next/navigation";
 
-export default function Activity() {
+export default function Activity({ params }: ActivityProps) {
+  const router = useRouter();
   const [events, setEvents] = useState<Event[]>([]);
+  const activeEvent = params.activityId?.length ? events.find(event => event.id === (params.activityId?.[0])) : undefined;
 
   useEffect(() => {
     getAllActivity().then(result => {
@@ -14,14 +18,25 @@ export default function Activity() {
     })
   }, []);
 
+  const closeActiveEvent = () => {
+    router.push('/activity');
+  }
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>All Activity</h1>
-      {events.map(event => (
-        <div key={event.id} className={styles.eventContainer}>
-          <EventSummary event={event} />
-        </div>
-      ))}
+      <div className={styles.activityContainer}>
+        <h1 className={styles.title}>All Activity</h1>
+        {events.map(event => (
+          <div key={event.id} className={`${styles.eventContainer} ${event.id === activeEvent?.id ? styles.activeEvent : ''}`}>
+            <EventSummary event={event} />
+          </div>
+        ))}
+      </div>
+      {activeEvent && <EventSidebar event={activeEvent} onClose={closeActiveEvent} />}
     </div>
   );
 }
+
+type ActivityProps = {
+  params: { activityId?: string[] };
+};
